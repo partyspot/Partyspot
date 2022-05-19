1a972eec54906dff0061d26a052731b2b7349d9f
Grubsi13 <13grubsi13@gmail.com>
2022-05-19 09:28:00 +0200

    first attempt at login component

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: ''
   }
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent }
+    ])
   ],
-  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
