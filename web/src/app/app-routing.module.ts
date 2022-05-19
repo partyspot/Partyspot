de5324d8028f67fdbb97a31ea820b55405bae582
Grubsi13 <13grubsi13@gmail.com>
2022-05-19 11:09:28 +0200

    added overview and tried to install font-awesome

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { OverviewComponent } from './components/overview/overview/overview.component';
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', component: LoginComponent }
+      { path: '', component: LoginComponent },
+      { path: 'overview', component: OverviewComponent },
     ])
   ],
 })
