6ef211555dd962d831289604dd6cfc52eb2b6603
Larsifur <j.weghi@gmail.com>
2022-05-23 15:30:18 +0200

    base for java web service

---
+++ server/Backend/src/main/java/at/partyspot/rest/resources/TestResource.java
@@ -0,0 +1,28 @@
+package at.partyspot.rest.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/testpath/")
+public class TestResource {
+	
+	@Context
+    private UriInfo context;
+ 
+    /** Creates a new instance of HelloWorld */
+    public TestResource() {
+    }
+	
+    
+	@Path("/testfunction/")
+	@GET
+	@Produces("text/plain")
+	public Response getTestString() {
+		return Response.ok().entity("Hello World!").build();
+	}
+	
+}
