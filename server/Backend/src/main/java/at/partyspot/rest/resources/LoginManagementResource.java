0dce6f79eee3c54d76363ce4944a2586ab1c8d04
Larsifur <j.weghi@gmail.com>
2022-06-02 08:13:33 +0200

    added codeGenerator and some Login logic

--- server/Backend/src/main/java/at/partyspot/rest/resources/LoginManagementResource.java
+++ server/Backend/src/main/java/at/partyspot/rest/resources/LoginManagementResource.java
@@ -1,27 +1,26 @@
 package at.partyspot.rest.resources;
 
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import at.partyspot.Generators.CodeGenerator;
+
 // manages login according to userrole
 @Path("/login")
 public class LoginManagementResource {
-	
-	@Inject
-	SpotifyAPITokenManager spotifyAPITokenManager;
 	
 	
 	@Path("newHost")
 	@POST
 	@Consumes("text/plain")
-	public Response createNew(@QueryParam("credentials") String credentials, @QueryParam("partycode") String partycode) throws Exception {
-		if (spotifyAPITokenManager.checkCredentials(credentials)) {
-			// Services create new AdminUser and Party, note that Partycode has to be defined prior by adminuser
-			return Response.ok().entity("New host started a party!").build();
+	public Response createNew(@QueryParam("credentials") String credentials) throws Exception {
+		if (SpotifyAPITokenManager.checkCredentials(credentials)) {
+			// Services create new AdminUser and Party
+			String userCode = CodeGenerator.generateUserCode();
+			return Response.ok().entity(userCode).build();
 		} else {
 			return Response.serverError().build();
 		}
