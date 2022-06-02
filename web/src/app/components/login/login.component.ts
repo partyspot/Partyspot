541b3455841f026e43fae793c31af819db52f44f
Grubsi13 <13grubsi13@gmail.com>
2022-06-02 09:15:57 +0200

    login input gets passed to ts file

--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 
@@ -9,11 +9,23 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  @ViewChild('username')
+  username: ElementRef;
+
+  @ViewChild('password')
+  password: ElementRef;
+
+  @ViewChild('guestCode')
+  guestCode: ElementRef;
+
   constructor(private router: Router) { }
 
   ngOnInit() { }
 
   login(): void {
+    console.log(this.username.nativeElement.value);
+    console.log(this.password.nativeElement.value);
+    console.log(this.guestCode.nativeElement.value);
     this.router.navigate(['/overview']);
   }
 
