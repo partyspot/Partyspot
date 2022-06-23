2fb31106493674c26f9d9f1d02217f4a623905ff
11haindl <69724357+11haindl@users.noreply.github.com>
2022-06-23 13:53:03 +0200

    Adaptions vor Deployment

--- web/src/app/rest/restService/restService.ts
+++ web/src/app/rest/restService/restService.ts
@@ -8,51 +8,59 @@ import { Song } from '../DTOModels/Song';
 export class RestService {
 
     queryParams: any;
+    root: string;
 
     options = {
         responseType: 'text' as const,
     };
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { 
+
+        if (window.location.hostname === 'localhost') {
+            this.root = 'http://localhost:8080/Backend/';
+          } else {
+            this.root = window.location.origin + '/Backend/';
+          }
+    }
 
     async adminLogin(): Promise<string> {
-        return this.http.get('http://localhost:8080/Backend/rest/login/loginWithSpotify', this.options).toPromise();
+        return this.http.get(this.root + 'rest/login/loginWithSpotify', this.options).toPromise();
     }
 
     async guestLogin(guestCode: string, guestName: string): Promise<string> {
-        return this.http.get('http://localhost:8080/Backend/rest/login/guestLogin?guestCode=' + guestCode + '&username=' + guestName, this.options).toPromise();
+        return this.http.get(this.root + 'rest/login/guestLogin?guestCode=' + guestCode + '&username=' + guestName, this.options).toPromise();
     }
 
     async createNewPartyWithNewCodeAndHostAndGetGuestCode(code: string): Promise<string> {
-        return this.http.get('http://localhost:8080/Backend/rest/login/newPartyAndNewHost?code=' + code, this.options).toPromise();
+        return this.http.get(this.root + 'rest/login/newPartyAndNewHost?code=' + code, this.options).toPromise();
     }
 
     async getDefaultPlaylist(adminId: UUID) {
-        return this.http.get('http://localhost:8080/Backend/rest/db/getDefaultPlaylist?adminId=' + adminId.toString(), this.options).toPromise();
+        return this.http.get(this.root + 'rest/db/getDefaultPlaylist?adminId=' + adminId.toString(), this.options).toPromise();
     }
 
     async getSearchResult(searchString: string, userId: UUID) {
-        return this.http.get('http://localhost:8080/Backend/rest/party/searchSongs?searchString=' + searchString + '&userId=' + userId.toString()).toPromise();
+        return this.http.get(this.root + 'rest/party/searchSongs?searchString=' + searchString + '&userId=' + userId.toString()).toPromise();
     }
 
     async addSongToPlaylist(song: Song, userId: UUID) {
-        return this.http.post('http://localhost:8080/Backend/rest/party/addSong?userId=' + userId.toString(), song).toPromise();
+        return this.http.post(this.root + 'rest/party/addSong?userId=' + userId.toString(), song).toPromise();
     }
 
     async getTokenWithPartyCode(partycode: string) {
-        return this.http.get('http://localhost:8080/Backend/rest/party/getPartyToken?partycode=' + partycode, this.options).toPromise();
+        return this.http.get(this.root + 'rest/party/getPartyToken?partycode=' + partycode, this.options).toPromise();
     }
 
     async getVotingView(userId: string) {
-        return this.http.get('http://localhost:8080/Backend/rest/party/getVotingView?userId=' + userId).toPromise();
+        return this.http.get(this.root + 'rest/party/getVotingView?userId=' + userId).toPromise();
     }
 
     async updateSongVoting(songId: string, voteSetting: string) {
-        return this.http.post('http://localhost:8080/Backend/rest/party/updateVotingView?songId=' + songId + '&voteSetting=' + voteSetting, "").toPromise();
+        return this.http.post(this.root + 'rest/party/updateVotingView?songId=' + songId + '&voteSetting=' + voteSetting, "").toPromise();
     }
 
     async getInviteCodeForUser(userId: string) {
-        return this.http.get('http://localhost:8080/Backend/rest/party/getInviteCodeForUser?userId=' + userId, this.options).toPromise();
+        return this.http.get(this.root + 'rest/party/getInviteCodeForUser?userId=' + userId, this.options).toPromise();
     }
 
 }
