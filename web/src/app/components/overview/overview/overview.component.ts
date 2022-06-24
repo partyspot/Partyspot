92596a77d7f3c022a9ca4ade32f58d645f35c254
Larsifur <j.weghi@gmail.com>
2022-06-24 14:10:52 +0200

    adding comments to overview.ts

--- web/src/app/components/overview/overview/overview.component.ts
+++ web/src/app/components/overview/overview/overview.component.ts
@@ -14,7 +14,7 @@ import { Song } from 'src/app/rest/DTOModels/Song';
 import { Track } from 'src/app/rest/DTOModels/track';
 import { VotingView } from 'src/app/rest/DTOModels/votingview';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { Subject, Subscription, timer } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-overview',
@@ -47,31 +47,38 @@ export class OverviewComponent implements OnInit {
   @ViewChild('songSearch')
   songSearch: ElementRef;
 
+  // in the constructor we need to inject different services, the Modal Controller and Router
   constructor(public modalCtrl: ModalController, private router: Router, private restService: RestService,
     private stateService: StateService, private alertService: AlertService, private sanitizer: DomSanitizer) {
+    // here we construct the spotify-embed URL for the player; the two Basis are constant wheras the id is meant to be exchanged
     this.songUrlBasis1 = 'https://open.spotify.com/embed/track/';
     this.songUrlBasis2 = '?utm_source=generator&theme=0&autoplay=1&muted=0&autopause=0';
+    // we set a default song for initializing the page
     let id = '3jBLKVqnOcxeXaqdGZ0p45';
     this.songUrl = this.songUrlBasis1 + id + this.songUrlBasis2;
     this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.songUrl);
+    // this is an attempt at controlling the player to enable autoplay
     window.addEventListener("blur", () => {
       setTimeout(() => {
         if (document.activeElement.tagName === "IFRAME") {
-          console.log("clicked");
+          //document.activeElement.;
         }
       });
     }, { once: false });
   }
 
   async ngOnInit() {
+    // the function that controlls, what is happening after login on the page
     await this.onPageLoad();
-    // timer call the function immediately and every 5 seconds 
+    // every second it is checked whether an internal updateToken has changed.
+    // If it is changed in the localStorage on certain events the component will register it and update the shown playlist
     setInterval(() => {
       if (this.needsPlaylistViewUpdateToken !== localStorage.getItem(this.inviteCode)) {
         this.needsPlaylistViewUpdateToken = localStorage.getItem(this.inviteCode);
         this.getVotingView();
       }
     }, 1000);
+    // the following is connected to the attempt of enabling autoplay of the player
     await this.waitForSpotifyWebPlaybackSDKToLoad();
     let token;
     await this.restService.getTokenWithPartyCode(this.inviteCode).then(res => {
@@ -89,7 +96,6 @@ export class OverviewComponent implements OnInit {
     if (connected) {
       await sdk.resume();
       await sdk.setVolume(0.5);
-      console.log("player connected!");
     }
   }
 
@@ -121,7 +127,10 @@ export class OverviewComponent implements OnInit {
   }
 
   async onPageLoad() {
+    // as after login we are staying in the same session we can use the sessionStorage to determine whether an admin or guest has logged in
     if (sessionStorage.getItem("isAdmin") === "n") {
+      // as a guest we want to set isAdmin to false and get the inviteCode of the party they have logged in, which is used later on
+      // we also refresh the shown playlist for the guest after login
       this.isAdmin = false;
       await this.restService.getInviteCodeForUser(sessionStorage.getItem("currentUser")).then(res => {
         this.inviteCode = res;
@@ -131,6 +140,7 @@ export class OverviewComponent implements OnInit {
       });
     }
     if (sessionStorage.getItem("isAdmin") !== "n") {
+      // in an admin login we have to check the location parameter of the browser window, later needed for retrieving the spotify code
       if (window.location.search.length > 0 && sessionStorage.getItem("isAdmin") === "y") {
         await this.handleRedirect();
       } else {
@@ -140,6 +150,8 @@ export class OverviewComponent implements OnInit {
     }
   }
 
+  // this function is meant for admin login specific, we get the spotify code from the query parameter of the spotify redirect uri
+  // and add the adminId connected to the current session to the stateService, to keep track of all logged in hosts.
   async handleRedirect() {
     this.spotifyCode = this.getCode();
     this.isAdmin = true;
@@ -150,14 +162,16 @@ export class OverviewComponent implements OnInit {
       const adminId = results[1].replaceAll('-', '')
       this.stateService.addAdminId(this.currentSessionId, toUUID(adminId));
       localStorage.setItem(this.inviteCode, UUID.UUID().toString());
+      // the defaultPlaylist is created in spotify and can be used and modified in the spotify account or via the spotify API.
+      // It is not used yet because we handle the playlist in our own database but can be used to improve playback-features in the future.
       this.restService.getDefaultPlaylist(results[1]).then(res => {
         this.restDefaultPlaylist = res;
-        console.log(res);
       });
 
     });
   }
 
+  //here we extract the spotify code spotify provided as query parameter in the redirectURI
   getCode() {
     let code = null;
     const queryString = window.location.search;
@@ -168,6 +182,7 @@ export class OverviewComponent implements OnInit {
     return code;
   }
 
+  // triggers the spotify search in backend and returns the search results that spotify delivers converted to Song-Objects, used in Frontend
   async getSearchResults(searchString: string) {
     if (searchString.length > 2) {
       if (this.isAdmin) {
@@ -181,11 +196,11 @@ export class OverviewComponent implements OnInit {
           this.searchResults = res as Song[];
         });
       }
-      console.log(this.searchResults);
       return this.searchResults;
     }
   }
 
+  // adding selected song to playlist, determined by the userId (which is identically linked to the party)
   async addSong(song: Song) {
     let userId;
     if (this.isAdmin) {
@@ -200,13 +215,12 @@ export class OverviewComponent implements OnInit {
     //song.name = 'Testsong';
     //song.spotifyUri = '1234567890123:7xGfFoTpQ2E7fRF5lN10tr';
     //const song = this.searchResults[0];
-    const jsonsong = JSON.stringify(song);
     await this.restService.addSongToPlaylist(song, userId).then(res => {
-      console.log("Song added");
       localStorage.setItem(this.inviteCode, UUID.UUID().toString());
     });
   }
 
+  // the refresh-function for reading the current state of the playlist in database and updating shown playlist and player accordingly
   getVotingView() {
     let userId;
     if (this.isAdmin) {
@@ -220,6 +234,8 @@ export class OverviewComponent implements OnInit {
     });
   }
 
+  // resetting the player to select the first element of the playlist, after playing the song that song is removed from playlist,
+  // therefore it only needs to select the first element
   refreshPlayer() {
     if (this.rows[0]) {
       const parsedSpotifyUri = this.parseSongUri(this.rows[0].song.spotifyUri);
@@ -228,22 +244,24 @@ export class OverviewComponent implements OnInit {
     }
   }
 
+  // the uri returned by the spotify API is not quite identical to the correct song URI, therefore some processing is needed
   parseSongUri(completeUri: string) {
     return completeUri.substring(14);
   }
 
+  // adding +1 to the voting of one song
   upVote(value) {
     this.voteSetting = 1;
-    console.log("upVote")
     this.vote(value);
   }
 
+  // removing 1 from the voting of one song
   downVote(value) {
     this.voteSetting = -1;
-    console.log("downVote")
     this.vote(value);
   }
 
+  // generic voting function reading voteSetting to know whether song should be up- or downvoted and triggering update in database
   async vote(value) {
     console.log(value.id);
     let userId;
@@ -255,11 +273,13 @@ export class OverviewComponent implements OnInit {
     console.log(userId);
     if (this.voteSetting) {
       await this.restService.updateSongVoting(value.id, userId, this.voteSetting.toString()).then(res => {
+        // the signal that is used so that every user instance knows that it should reload the shown playlist from the database
         localStorage.setItem(this.inviteCode, UUID.UUID().toString());
       });
     }
   }
 
+  // another function as an attempt to get the autoplay working for the player
   async waitForSpotifyWebPlaybackSDKToLoad() {
     return new Promise(resolve => {
       if (window.Spotify) {
@@ -272,8 +292,8 @@ export class OverviewComponent implements OnInit {
     });
   };
 
+  // parent function after song is selected to being added to playlist
   addThisSong(result: Song) {
-    console.log(result);
     this.addSong(result);
     this.resetSearch();
   }
@@ -284,6 +304,7 @@ export class OverviewComponent implements OnInit {
     this.songSearch.nativeElement.value = '';
   }
 
+  // the function used after playback of song has finished or admin wants to skip the song, thus the next song is played on the playlist
   async skipSong() {
     await this.restService.deleteSong(this.rows[0].song.id.toString()).then(res => {
       localStorage.setItem(this.inviteCode, UUID.UUID().toString());
